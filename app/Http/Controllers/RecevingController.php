6ecd7be526c9ac9ad4fd8484ba6c8a36cbeab7b1
Nitesh Devnath <42641530+blackvikings@users.noreply.github.com>
2020-04-03 22:20:30 +0530

    db

---
+++ app/Http/Controllers/RecevingController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Receving;
+use Illuminate\Http\Request;
+
+class RecevingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Receving  $receving
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Receving $receving)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Receving  $receving
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Receving $receving)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Receving  $receving
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Receving $receving)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Receving  $receving
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Receving $receving)
+    {
+        //
+    }
+}
