6f08972f0488199faeb1fb6b27b3246c1f66293a
Nitesh Devnath <42641530+blackvikings@users.noreply.github.com>
2020-04-04 11:37:18 +0530

    miration added

--- database/migrations/2020_04_03_124229_create_bills_table.php
+++ database/migrations/2020_04_03_124229_create_bills_table.php
@@ -15,6 +15,41 @@ class CreateBillsTable extends Migration
     {
         Schema::create('bills', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->date('billdate');
+            $table->integer('billserial');
+            $table->string('billid', 250);
+            $table->unsignedInteger('party_id')->unsigned(); 
+            $table->foreign('party_id')->refernces('id')->on('parties');
+            $table->float('partypercentnew', '5', '2');
+            $table->string('itemname');
+            $table->float('beforemeltingweight', '5', '3');
+            $table->float('aftermeltingweight', '5', '3');
+            $table->float('sampleweight','5','3');
+            $table->float('receivedweight');
+            $table->float('fireassayweight', '5', '3');
+            $table->float('refineweight', '5', '3');
+            $table->float('assaypurity','5', '3');
+            $table->float('cgst', '5', '2');
+            $table->float('sgst', '5', '2');
+            $table->text('totalamount');
+            $table->text('remark');
+            $table->text('clientpurity');
+            $table->text('batchnumber');
+            $table->text('puritydifference');
+            $table->text('refinecharge');
+            $table->text('puresample');
+            $table->text('refinedweight');
+            $table->text('pureweight');
+            $table->text('tothecustomer');
+            $table->text('gain');
+            $table->date('issueddate');
+            $table->text('fineweight');
+            $table->text('fineweightwithcharge');
+            $table->text('cointype');
+            $table->text('coinissuedweight');
+            $table->text('totalbalance');
+            $table->text('description');
+            $table->text('coinremark');
             $table->timestamps();
         });
     }
