090863d280ad94e12e81bf840e7705a7b780690a
Nitesh Devnath <42641530+blackvikings@users.noreply.github.com>
2020-04-24 23:13:40 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -26,61 +26,61 @@ Route::group(['middleware' => ['auth']], function() {
    	Route::resource('tags', 'TagController');
 });
 
-Route::get('/home', 'HomeController@index')->name('home');
-Route::post('/role-assign', 'UserController@roleassign')->name('role-assign');
+Route::get('/admin/home', 'HomeController@index')->name('home');
+Route::post('/admin/role-assign', 'UserController@roleassign')->name('role-assign');
 
-Route::get('/manage-user', 'UserController@index')->name('manage.user');
-Route::get('/create-user', 'UserController@create')->name('create.user.view');
-Route::post('/create-user', 'UserController@store')->name('create.user');
-Route::get('/edit-user/{id}', 'UserController@edit')->name('edit-user');
-Route::patch('/edit-user/{id}', 'UserController@update')->name('update.user');
-Route::delete('/delete-user/{id}', 'UserController@destroy')->name('delete.user');
+Route::get('/admin/manage-user', 'UserController@index')->name('manage.user');
+Route::get('/admin/create-user', 'UserController@create')->name('create.user.view');
+Route::post('/admin/create-user', 'UserController@store')->name('create.user');
+Route::get('/admin/edit-user/{id}', 'UserController@edit')->name('edit-user');
+Route::patch('/admin/edit-user/{id}', 'UserController@update')->name('update.user');
+Route::delete('/admin/delete-user/{id}', 'UserController@destroy')->name('delete.user');
 
-Route::get('/roles', 'RolesController@index')->name('manage.roles');
-Route::get('/roles-create', 'RolesController@createview')->name('roles-create-view');
-Route::post('/roles-create', 'RolesController@create')->name('roles-create');
-Route::get('/roles-update/{role}', 'RolesController@updateview')->name('roles.update.view');
-Route::patch('/roles-update/{role}', 'RolesController@update')->name('roles.update');
-Route::delete('/roles-delete/{role}', 'RolesController@destroy')->name('roles.delete');
+Route::get('/admin/roles', 'RolesController@index')->name('manage.roles');
+Route::get('/admin/roles-create', 'RolesController@createview')->name('roles-create-view');
+Route::post('/admin/roles-create', 'RolesController@create')->name('roles-create');
+Route::get('/admin/roles-update/{role}', 'RolesController@updateview')->name('roles.update.view');
+Route::patch('/admin/roles-update/{role}', 'RolesController@update')->name('roles.update');
+Route::delete('/admin/roles-delete/{role}', 'RolesController@destroy')->name('roles.delete');
 
-Route::get('/roles-permissions/{slug}', 'RoleAndPermissionsController@index')->name('assign');    
-Route::post('/assign-permission', 'RoleAndPermissionsController@assign')->name('permissions-assign');
-Route::post('/unassign-permission', 'RoleAndPermissionsController@unassign')->name('permission.unassign');
+Route::get('/admin/roles-permissions/{slug}', 'RoleAndPermissionsController@index')->name('assign');    
+Route::post('/admin/assign-permission', 'RoleAndPermissionsController@assign')->name('permissions-assign');
+Route::post('/admin/unassign-permission', 'RoleAndPermissionsController@unassign')->name('permission.unassign');
 
-Route::get('/permissions', 'PermissionController@index')->name('manage.permissions');
-Route::get('/permission-create', 'PermissionController@createview')->name('permission.create.view');
-Route::post('/permission-create', 'PermissionController@create')->name('permission.create');
-Route::get('/permission-update/{permission}', 'PermissionController@updateview')->name('permission-update-view');
-Route::patch('/permission-update/{permission}', 'PermissionController@update')->name('permission-update');
-Route::delete('/permission-delete/{permission}', 'PermissionController@destroy')->name('permission.delete');
+Route::get('/admin/permissions', 'PermissionController@index')->name('manage.permissions');
+Route::get('/admin/permission-create', 'PermissionController@createview')->name('permission.create.view');
+Route::post('/admin/permission-create', 'PermissionController@create')->name('permission.create');
+Route::get('/admin/permission-update/{permission}', 'PermissionController@updateview')->name('permission-update-view');
+Route::patch('/admin/permission-update/{permission}', 'PermissionController@update')->name('permission-update');
+Route::delete('/admin/permission-delete/{permission}', 'PermissionController@destroy')->name('permission.delete');
 
-Route::get('/manage-party', 'PartyController@index')->name('manage.party');
-Route::post('/manage-party', 'PartyController@store')->name('store.party');
-Route::post('/edit-party', 'PartyController@edit')->name('edit.party');
-Route::post('/update-party', 'PartyController@update')->name('update.party');
+Route::get('/admin/manage-party', 'PartyController@index')->name('manage.party');
+Route::post('/admin/manage-party', 'PartyController@store')->name('store.party');
+Route::post('/admin/edit-party', 'PartyController@edit')->name('edit.party');
+Route::post('/admin/update-party', 'PartyController@update')->name('update.party');
 
-Route::get('/receiving', 'BillController@index')->name('receiving');
-Route::post('/party-parameter', 'BillController@partyParameter')->name('party.parameter');
-Route::post('/bill-store', 'BillController@store')->name('bill.store');
+Route::get('/admin/receiving', 'BillController@index')->name('receiving');
+Route::post('/admin/party-parameter', 'BillController@partyParameter')->name('party.parameter');
+Route::post('/admin/bill-store', 'BillController@store')->name('bill.store');
 
-Route::get('/edit-receiving', 'BillController@edit')->name('edit.receiving');
-Route::post('/get-bill', 'BillController@show')->name('bill.edit');
-Route::post('/get-bill-details', 'BillController@fullDetails')->name('full.details');
-Route::post('/update-bill/{billid}', 'BillController@update')->name('update.bill');
+Route::get('/admin/edit-receiving', 'BillController@edit')->name('edit.receiving');
+Route::post('/admin/get-bill', 'BillController@show')->name('bill.edit');
+Route::post('/admin/get-bill-details', 'BillController@fullDetails')->name('full.details');
+Route::post('/admin/update-bill/{billid}', 'BillController@update')->name('update.bill');
 
-Route::get('/fireassay', 'FireassayController@index')->name('fireassay');
-Route::post('fireassay-bills', 'FireassayController@create')->name('fireassay.bills');
+Route::get('admin/fireassay', 'FireassayController@index')->name('fireassay');
+Route::post('admin/fireassay-bills', 'FireassayController@create')->name('fireassay.bills');
 
-Route::get('/refine', 'RefineController@index')->name('refine');
-Route::post('/refine-single', 'RefineController@create')->name('refine.single');
-Route::post('/refine-update', 'RefineController@update')->name('update.bill');
-Route::post('/load-batch', 'RefineController@batch')->name('load.batch');
-Route::post('/total-batch', 'RefineController@loadbatch')->name('total.batch');
-Route::post('/save-refine', 'RefineController@store')->name('save.refine');
+Route::get('/admin/refine', 'RefineController@index')->name('refine');
+Route::post('/admin/refine-single', 'RefineController@create')->name('refine.single');
+Route::post('/admin/refine-update', 'RefineController@update')->name('update.bill');
+Route::post('/admin/load-batch', 'RefineController@batch')->name('load.batch');
+Route::post('/admin/total-batch', 'RefineController@loadbatch')->name('total.batch');
+Route::post('/admin/save-refine', 'RefineController@store')->name('save.refine');
 
-Route::get('/stock', 'StockController@index')->name('stock');
+Route::get('/admin/stock', 'StockController@index')->name('stock');
 
-Route::get('/ledger', 'LedgerController@index')->name('ledger');
+Route::get('/admin/ledger', 'LedgerController@index')->name('ledger');
 
 Route::get('/clear-cache', function() {
     Artisan::call('cache:clear');
